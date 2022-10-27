b06449f794e4e7910c60b89902a7cff98a270c4a
Vladimir <vladmir.sidorenko@gmail.com>
2022-10-27 14:19:12 +0300

    Release v 1.0

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        long minors = persons.stream()
+                .filter(person -> person.getAge() <18)
+                .count();
+        System.out.println(minors);
+
+        List <String> conscript = persons.stream()
+                .filter(person -> person.getSex().equals(Sex.MAN))
+                .filter(person -> person.getAge()>18)
+                .filter(person -> person.getAge()<27)
+                .map(person -> person.getFamily())
+                .collect(Collectors.toList());
+        System.out.println(conscript);
+
+        List <String> higherEducation = persons.stream()
+                .filter(person -> person.getEducation().equals(Education.HIGHER))
+                .filter(person -> person.getAge() >=18)
+                .filter(person -> (person.getAge() <= 65 & person.getSex().equals(Sex.MAN)) | (person.getAge() <= 60 & person.getSex().equals(Sex.WOMAN)))
+                .sorted(Comparator.comparing(Person::getFamily))
+                .map(person -> "\n" + person.toString())
+                .collect(Collectors.toList());
+
+        System.out.println(higherEducation);
+    }
+}
